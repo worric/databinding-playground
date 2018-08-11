89ea73791832c05bd2f63394ef74e795cdcbb64b
Robin Damsgaard Larsen <rdalarsen@gmail.com>
2018-08-11 23:24:53 +0200

    test: Verify views are displayed and processing of text works

--- app/src/androidTest/java/me/worric/databindingplayground/MainActivityTest.java
+++ app/src/androidTest/java/me/worric/databindingplayground/MainActivityTest.java
@@ -8,23 +8,45 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.*;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
-    private static final String TEXT_VIEW_TEXT_HELLO_WORLD = "Hello World!";
+    private static final String INPUT_TEXT = "test";
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityActivityTestRule =
             new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void onAppLaunch_textIsShown() {
+    public void onAppLaunch_inputEditTextIsShown() {
+        onView(withId(R.id.et_input_text))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void onAppLaunch_buttonIsShown() {
+        onView(withId(R.id.btn_process_text))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void onAppLaunch_outputTextViewIsShown() {
         onView(withId(R.id.tv_output_text))
-                .check(matches(withText(TEXT_VIEW_TEXT_HELLO_WORLD)));
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void onButtonPress_processInputTextAndShowOutputText() {
+        onView(withId(R.id.et_input_text))
+                .perform(typeText(INPUT_TEXT));
+        onView(withId(R.id.btn_process_text))
+                .perform(click());
+        onView(withId(R.id.tv_output_text))
+                .check(matches(withText(INPUT_TEXT)));
     }
 
 }
