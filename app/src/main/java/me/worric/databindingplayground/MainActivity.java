fefc7b48a532b6fa21dbb041662595387afdac31
Robin Damsgaard Larsen <rdalarsen@gmail.com>
2018-08-12 00:06:33 +0200

    misc: Add MainViewModel and adapt MainActivity and layout accordingly

--- app/src/main/java/me/worric/databindingplayground/MainActivity.java
+++ app/src/main/java/me/worric/databindingplayground/MainActivity.java
@@ -1,5 +1,6 @@
 package me.worric.databindingplayground;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,18 +12,22 @@ import me.worric.databindingplayground.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding mBinding;
+    private MainViewModel mViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mBinding.setViewmodel(mViewModel);
+        mBinding.setLifecycleOwner(this);
     }
 
     public void handleProcessText(View view) {
         String textFromEditText = mBinding.etInputText.getText().toString();
 
         if (!TextUtils.isEmpty(textFromEditText)) {
-            mBinding.tvOutputText.setText(textFromEditText);
+            mViewModel.setOutputText(textFromEditText);
         }
     }
 
