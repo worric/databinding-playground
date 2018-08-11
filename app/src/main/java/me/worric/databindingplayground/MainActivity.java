4100038ca9686cedc671153d83f9e0952f46f0e5
Robin Damsgaard Larsen <rdalarsen@gmail.com>
2018-08-11 22:19:49 +0200

    misc: Implement databinding in MainActivity

--- app/src/main/java/me/worric/databindingplayground/MainActivity.java
+++ app/src/main/java/me/worric/databindingplayground/MainActivity.java
@@ -1,13 +1,19 @@
 package me.worric.databindingplayground;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import me.worric.databindingplayground.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
+
 }
