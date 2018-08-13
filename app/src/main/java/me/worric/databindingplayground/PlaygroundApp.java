dd81fd3ca50a28ee50a363b42b5b8d29382c78e3
Robin Damsgaard Larsen <rdalarsen@gmail.com>
2018-08-13 20:43:46 +0200

    misc: Initialize Stetho in Manifest

---
+++ app/src/main/java/me/worric/databindingplayground/PlaygroundApp.java
@@ -0,0 +1,15 @@
+package me.worric.databindingplayground;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+public class PlaygroundApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initializeWithDefaults(this);
+    }
+
+}
